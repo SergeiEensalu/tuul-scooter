6fae1eb428482f3a8f929c886535ea5467bc8d54
Sergei Eensalu <sergei.eensalu@gmail.com>
2025-05-27 02:37:42 +0300

    Comment

--- frontend/src/shared/ui/CenteredCard.tsx
+++ frontend/src/shared/ui/CenteredCard.tsx
@@ -6,9 +6,11 @@ type Props = {
 
 export const CenteredCard: React.FC<Props> = ({children}) => {
   return (
-    // Comment by Sergei:  actually this 'mt-32' should not be here,
-    // this component should be max be reusable and as simple as possible,
-    // but cause I do not have time to solve problem with general layout and page sizes and elements - let be here.
+      // Comment by Sergei: Ideally, this 'mt-32' shouldn't be here.
+      // My philosophy is that all reusable components must stay truly REUSABLE,
+      // meaning they should not contain layout-specific hacks or case-specific styling.
+      // Unfortunately, due to time constraints and unresolved layout/page structure issues,
+      // I'm intentionally breaking this principle here for now. Let it be.
 
     <div className="flex justify-center mt-32">
       <div className="p-6 space-y-4 max-w-md w-full border rounded-xl shadow-sm bg-white">
